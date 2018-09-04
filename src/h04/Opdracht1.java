d4d4e6de422c48472be8afcbb12f42227837f6ca
stanley <stanley2001@live.nl>
2018-09-04 12:28:07 +0200

    Opdrach H4.1

---
+++ src/h04/Opdracht1.java
@@ -0,0 +1,18 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.drawArc(100, 100, 200, 200, 800, 25);
+        g.fillArc(100, 100, 200, 200, 800, 25);
+
+    }
+}

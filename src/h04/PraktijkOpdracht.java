fdf30b58577b9c6d77a3b25fd44a25e13aa5ad67
stanley <stanley2001@live.nl>
2018-09-05 14:25:21 +0200

    Opdrachten geüpdate.

--- src/h04/PraktijkOpdracht.java
+++ src/h04/PraktijkOpdracht.java
@@ -5,65 +5,60 @@ import java.applet.*;
 
 public class PraktijkOpdracht extends Applet {
 
+    Color oranje;
+    Color roze;
+    int breedte;
+    int hoogte;
+
     public void init() {
+        oranje = new Color(244, 80, 66);
+        breedte = 100;
+        roze = new Color(244, 65, 244);
+        hoogte = 50;
     }
 
     public void paint(Graphics g) {
         setBackground(Color.WHITE);
         g.setColor(Color.black);
         g.drawLine(10, 10, 200, 10);
-        g.drawString("Lijn", 100, 25);
-        g.drawRect(20, 50, 100, 50);
+        g.drawString("Lijn", breedte, 25);
+        g.drawRect(20, 50, breedte, hoogte);
         g.drawString("Rechthoek", 40, 115);
 
-        g.setColor(Color.orange);
-        g.drawRect(160, 50, 100, 50);
-        g.fillRect(160, 50, 100, 50);
+        g.setColor(roze);
+        g.drawRect(160, 50, breedte, hoogte);
+        g.fillRect(160, 50, breedte, hoogte);
 
         g.setColor(Color.black);
-        g.drawArc(160, 50, 100, 50, 90, 360);
+        g.drawArc(160, 50, breedte, hoogte, 90, 360);
         g.drawString("Gevulde rechthoek met ovaal", 140, 115);
 
 
 
         g.setColor(Color.black);
-        g.drawArc(350, 50, 100, 50, 90, 360);
+        g.drawArc(350, 50, breedte, hoogte, 90, 360);
         g.drawString("Taartpunt met ovaal eromheen", 320, 115);
-        g.setColor(Color.orange);
-        g.drawArc(350, 50, 100, 50, 360, 45);
-        g.fillArc(350, 50, 100, 50, 360, 45);
+        g.setColor(roze);
+        g.drawArc(350, 50, breedte, hoogte, 360, 45);
+        g.fillArc(350, 50, breedte, hoogte, 360, 45);
 
         g.setColor(Color.black);
-        g.drawRoundRect(20, 150, 100, 50, 30, 30);
+        g.drawRoundRect(20, 150, breedte, hoogte, 30, 30);
         g.drawString("Afgeronde rechthoek", 20, 220);
 
-        g.setColor(Color.orange);
-        g.drawArc(160, 150, 100, 50, 90, 360);
-        g.fillArc(160, 150, 100, 50, 90, 360);
+        g.setColor(roze);
+        g.drawArc(160, 150, breedte, hoogte, 90, 360);
+        g.fillArc(160, 150, breedte, hoogte, 90, 360);
 
         g.setColor(Color.black);
         g.drawString("Gevulde ovaal", 170, 220);
 
 
         g.setColor(Color.black);
-        g.drawOval(375, 150, 50, 50);
+        g.drawOval(375, 150, 50, hoogte);
         g.drawString("Cirkel", 385, 220);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
+
 }

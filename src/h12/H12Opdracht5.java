d8439decd993bc8ffcd009fd789227e85b53df9c
stanley <stanley2001@live.nl>
2018-11-05 11:13:44 +0100

    Opdracht 4 done.

---
+++ src/h12/H12Opdracht5.java
@@ -0,0 +1,25 @@
+package h12;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class H12Opdracht5 extends Applet {
+
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+
+        }
+    }
+}
+
